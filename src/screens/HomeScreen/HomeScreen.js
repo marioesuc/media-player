e110ba8ea268c07921f6cea13a2b6e46ab476ad8
Mario Esteban Ucles <mario.esteban@posteo.de>
2019-07-09 08:23:55 +0200

    feat: save api data into local state

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -10,30 +10,50 @@ const api = create({
 
 const API_KEY = "a16910bed086b5e775c155b974f13eae";
 
-const doRequest = (url, apiKey, params) =>
-  api.get(`${url}?api_key=${apiKey}${params || ""}`);
+const doRequest = async (url, apiKey, params) => {
+  return await api
+    .get(`${url}?api_key=${apiKey}${params || ""}`)
+    .catch(error => console.log(error));
+};
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    };
+  }
+
   componentDidMount() {
-    doRequest("/3/trending/movie/day", API_KEY).then(response =>
-      console.log(response)
+    this.loadInitialData();
+  }
+
+  loadInitialData = () => {
+    const data = {};
+
+    doRequest("/3/trending/movie/day", API_KEY).then(
+      response => (data.trendingMovies = response.data.results)
     );
-    doRequest("/3/trending/tv/day", API_KEY).then(response =>
-      console.log(response)
+
+    doRequest("/3/trending/tv/day", API_KEY).then(
+      response => (data.trendingSeries = response.data.results)
     );
     doRequest(
       "/3/discover/movie",
       API_KEY,
       "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=10751"
-    ).then(response => console.log(response));
+    ).then(response => (data.family = response.data.results));
     doRequest(
       "/3/discover/movie",
       API_KEY,
       "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=99"
-    ).then(response => console.log(response));
-  }
+    ).then(response => (data.documentary = response.data.results));
+
+    this.setState({ data });
+  };
 
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
